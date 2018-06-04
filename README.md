5010a5721d3e9c4701c47e88029c56f1d2ccfd56
Jason Ware <jason@jknsware.com>
2018-06-04 12:27:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# EFK Proof of Concept
+
+
+1. Open `hello-world` [http://localhost:3000](http://localhost:3000).
+
+1. Open Kabana [http://localhost:5601](http://localhost:5601). 
+
+  Click the “Set up index patters” button in the upper-right corner of the screen. Kibana will start a wizard that guides you through configuring the data sets to visualize. If you want a quick start, use `logstash-*` as the index pattern, and select `@timestamp` as the time-filter field.
---
+++ nodeapp/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var logger = require('fluent-logger');
+var app = express();
+
+// The 2nd argument can be omitted. Here is a default value for options.
+logger.configure('fluentd.test', {
+  host: 'localhost',
+  port: 24224,
+  timeout: 3.0,
+  reconnectInterval: 600000 // 10 minutes
+});
+
+app.get('/', function(request, response) {
+  logger.emit('follow', {from: 'userA', to: 'userB'});
+  response.send('Hello World!');
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
